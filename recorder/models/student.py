781767f25741884e76d3ccd98d8359c5a2224d8a
Yi Zhou <zhouyi119119@gmail.com>
2019-08-29 14:28:45 +0800

    database ver0.4

--- recorder/models/student.py
+++ recorder/models/student.py
@@ -7,10 +7,11 @@ association_student_unit = db.Table('association_student_unit', db.Model.metadat
                                     )
 
 
-# association_teacher_unit = db.Table('association_teacher_unit', db.Model.metadata,
-#                                     db.Column('Teacher', db.Integer, db.ForeignKey('teacher.id')),
-#                                     db.Column('Unit', db.Integer, db.ForeignKey('unit.unit_id'))
-#                                     )
+association_student_assignment = db.Table('association_sutdent_assignment', db.Model.metadata,
+                                    db.Column('Student', db.Integer, db.ForeignKey('student.id')),
+                                    db.Column('Assignment', db.Integer, db.ForeignKey('assignment.id')),
+                                          tasks=db.relationship('Task', backref='assignment_tasks', lazy='dynamic')
+                                    )
 
 
 class Student(db.Model):
@@ -20,9 +21,10 @@ class Student(db.Model):
     last_name = db.Column(db.String(40))
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
-    unit = db.relationship('Unit', secondary=association_student_unit, backref='student_unit_owner', lazy='dynamic')
+    units = db.relationship('Unit', secondary=association_student_unit, backref='student_unit_owner', lazy='dynamic')
+    assignments = db.relationship('Unit', secondary=association_student_assignment, backref='student_assignment_owner', lazy='dynamic')
     # comments = db.relationship('Comment', backref='owner', lazy='dynamic')
-    records = db.relationship('Record', backref='student_records_owner', lazy='dynamic')
+    # records = db.relationship('Record', backref='student_records_owner', lazy='dynamic')
 
     def __repr__(self):
         return f'id={self.id}, student_number={self.student_number}, first_name={self.first_name}, last_name={self.last_name}, email={self.email},password_hash={self.password_hash}'
@@ -57,7 +59,7 @@ class Teacher(db.Model):
     last_name = db.Column(db.String(40))
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
-    unit = db.relationship('Unit', backref='unit_owner', lazy='dynamic')
+    units = db.relationship('Unit', backref='unit_owner', lazy='dynamic')
 
     # unit = db.relationship('Unit', secondary=association_teacher_unit, backref='teacher_unit_owner', lazy='dynamic')
 
