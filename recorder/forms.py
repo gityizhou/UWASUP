9dc84922a389814aeaeedc661a382430808ae068
Alex McAllister <alexandra.mcallister@hotmail.com>
2019-10-07 10:45:42 +0800

    work on form validations

--- recorder/forms.py
+++ recorder/forms.py
@@ -34,13 +34,13 @@ class RegisterForm(FlaskForm):
 
     username = StringField("Student Number / Staff Number", validators=[DataRequired(),
                                                                         Length(min=8, max=8,
-                                                                               message="UWA student/staff number should be 8 digits number.")])
+                                                                               message="UWA student/staff numbers should be 8 digits.")])
 
     firstname = StringField("First Name", validators=[DataRequired()])
     lastname = StringField("Last Name", validators=[DataRequired()])
     email = StringField("Email Address", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=8, max=18,
-                                                                            message="Length of password should longer than 7")])
+                                                                            message="Please use more than 7 characters.")])
     password2 = PasswordField(
         "Password Repeat", validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
@@ -49,44 +49,42 @@ class RegisterForm(FlaskForm):
         # check if your username is already existed in database
         user = User.query.filter_by(user_number=username.data).first()
         if user is not None:
-            raise ValidationError('existed staff/student number')
+            raise ValidationError('This student/staff number is already in use.')
 
     def validate_email(self, email):
         # this function will basicly split the address into 2 parts
         # check if your mail is UWA mail
         # check if your mail is already existed in database
         if "@" not in email.data:
-            raise ValidationError('please input a valid email address')
+            raise ValidationError('Please use a valid email address.')
         email_prefix = email.data.split("@")[0]
         email_suffix = email.data.split("@")[1]
 
         if email_suffix != "student.uwa.edu.au" and email_suffix != "uwa.edu.au":
-            raise ValidationError('please use uwa mail')
+            raise ValidationError('You must use a UWA email account.')
         if email_prefix != self.username.data:
-            raise ValidationError('please use your own uwa mail')
+            raise ValidationError('Please use your own student email.')
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError('existed email address')
+            raise ValidationError('This email address is already in use.')
 
 
-# form for students to subscribe themselves to units (student_view.html)
+# field needed for SubscribeUnitForm
 class MultiCheckboxField(SelectMultipleField):
     widget = ListWidget(prefix_label=False)
     option_widget = CheckboxInput()
-
 
 class SubscribeUnitForm(FlaskForm):
     subscribe_units = MultiCheckboxField('Units', [DataRequired(message='Please select one or more units.')],
                                          coerce=int)
     submit = SubmitField('Subscribe')
 
-
 class MakeTeacherForm(FlaskForm):
     userNumber = StringField('Staff Number', validators=[DataRequired()])
     submit = SubmitField('Make User a Teacher')
 
-    def validate_username(self):
-        user = User.query.filter_by(user_number=self.userNumber.data).first()
+    def validate_userNumber(self, userNumber):
+        user = User.query.filter_by(user_number=userNumber.data).first()
         if user is None:
             raise ValidationError('This user does not exist.')
 
@@ -94,22 +92,20 @@ class DeleteUserForm(FlaskForm):
     userNumber = StringField('User Number', validators=[DataRequired()])
     submit = SubmitField('Delete User')
 
-    def validate_username(self):
-        user = User.query.filter_by(user_number=self.userNumber.data).first()
+    def validate_userNumber(self, userNumber):
+        user = User.query.filter_by(user_number=userNumber.data).first()
         if user is None:
             raise ValidationError('This user does not exist.')
 
+# validators not needed in this form as form will only be generated for existing units
 class DeleteUnitForm(FlaskForm):
     unitID = StringField()
     submit = SubmitField('Delete Unit')
 
+# validators not needed in this form as form will only be generated for existing tasks
 class DeleteTaskForm(FlaskForm):
     taskID = StringField()
     submit = SubmitField('Delete Task')
-    def validate_username(self, userNumber):
-        user = User.query.filter_by(userNumber=self.userNumber.data).first()
-        if user is None:
-            raise ValidationError('This user does not exist.')
 
 class PasswdResetRequestForm(FlaskForm):
     email = StringField("Email Address", validators=[DataRequired(), Email()])
